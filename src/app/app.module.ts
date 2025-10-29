15882151896116b68a75fc9e67ac64ba69929c16
SahithiKasa <sahithikasa123.fullstack@gmail.com>
2025-10-29 23:52:14 +0530

    reactive approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TemplateApproachComponent } from './template-approach/template-approach.component';
+import { ReactiveApproachComponent } from './reactive-approach/reactive-approach.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateApproachComponent
+    TemplateApproachComponent,
+    ReactiveApproachComponent
   ],
   imports: [
-    BrowserModule,FormsModule,
+    BrowserModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
